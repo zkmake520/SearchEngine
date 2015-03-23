2b709966b64771ed38e604a6f632ee42e0c31efb
kanzhou <564254501@qq.com>
2015-03-23 14:13:26 -0700

    Modify the logic module of Indexer.py

--- Indexer.py
+++ Indexer.py
@@ -11,11 +11,13 @@ import sys
 from LangProc import docTerms
 # todo : remove this assumptions
 # Indexer assumes thast collection fits in memory()
-class Indexer:
+class DBMIndex:
+	pass
+class MemoryIndexer:
 	def __init__(self):
-		self.invertedIndex = dict()
+		self.invertedIndex = defaultdict(list())
 		self.forwardIndex = dict()
-		self.urlToId = dict()     #url is too long
+		self.idToUrl = dict()     #url is too long
 		self.docCount =0
 	# TOdo: remove this assumptions	
 	# assumes that adddocument () is never called twice for a document
@@ -24,11 +26,9 @@ class Indexer:
 	def addDocument(self,url,parsedText):
 		self.docCount += 1
 		currentId = self.docCount
-		self.urlToId[currentId] = url;
+		self.idToUrl[currentId] = url;
 		self.forwardIndex[currentId] = parsedText
 		for position,term in enumerate(parsedText):
-			if term not in self.invertedIndex:
-				self.invertedIndex[term] = []
 			self.invertedIndex[term].append((position,currentId))
 
 	# dump as json
@@ -37,28 +37,34 @@ class Indexer:
 			file = open(os.path.join(IndexDir,fileName),"w")
 			pickle.dump(source,file)
 
-		pickleDumpToFile(self.urlToId,"urlToId")
+		pickleDumpToFile(self.idToUrl,"idToUrl")
 		pickleDumpToFile(self.invertedIndex,"inverted")
 		pickleDumpToFile(self.forwardIndex,"forward")
 
-class Searcher():
-	def __init__(self,indexDir):
-		self.invertedIndex = dict()
-		self.forwardIndex = dict()
-		self.urlToId = dict()     #url is too long
-		self.docCount = 0
+	def loadFromDisk(self,indexDir):
 		def pickleLoadFromFile(fileName):
 			file = open(os.path.join(indexDir,fileName),"r")
 			return pickle.load(file)
-
 		self.invertedIndex=pickleLoadFromFile("inverted")
-		self.urlToId=pickleLoadFromFile("urlToId")
+		self.idToUrl=pickleLoadFromFile("idToUrl")
 		self.forwardIndex=pickleLoadFromFile("forward")
 
+	def getDocumentOfQuery(self,query):
+		return self.invertedIndex.get(query,[])
+
+	def getDocumentOfId(self,id):
+		return self.forwardIndex[id]
+
+	def getUrl(self,id):  # here we load all data from files thus the type is string !
+		return self.idToUrl[id]	
+			
+class Searcher():
+	def __init__(self,indexDir):
+		self.index = MemoryIndexer()
 	def findDocument_AND(self,queryStr):
 		documentIdList = defaultdict(lambda:0)
 		for term in queryStr:
-			for id in set([item[1] for item in self.invertedIndex.get(term,[])]):
+			for id in set([item[1] for item in self.index.getDocumentOfQuery(term)]):
 				documentIdList[id] += 1
 		return [docId for docId,cnt in documentIdList.iteritems() if cnt ==len(queryStr)]
 
@@ -68,27 +74,22 @@ class Searcher():
 		minWindow = []
 		minSize = sys.maxint
 		bestIndenticaltermSize = 0
-		for pos,term in enumerate(self.forwardIndex[id]):
+		for pos,term in enumerate(self.index.getDocumentOfId(id)):
 			if term in queryStr:
 				currentWindow[queryStr.index(term)] = pos
 				if  -1 not in currentWindow:
 					start = min(currentWindow)
 					end = pos
-					indenticaltermSize = len(set(self.forwardIndex[id][start : end+1])) 
+					indenticaltermSize = len(set(self.index.getDocumentOfId(id)[start : end+1])) 
 					if(minSize >  end-start+1) or (indenticaltermSize > bestIndenticaltermSize and minSize+2 >= end-start+1):
 						minWindow = currentWindow[:] 
 						minSize = end-start + 1 
 						bestIndenticaltermSize = indenticaltermSize
-		docLength = len(self.forwardIndex[id])
+		docLength = len(self.index.getDocumentOfId(id))
 		snippetsStart = max(min(minWindow)-10,0)
 		snippetsEnd = min(docLength, max(minWindow)+1+10)
-		return [(term.originalWord,term in queryStr) for term in self.forwardIndex[id][snippetsStart:snippetsEnd]] #excellent implemention:return list of truple make critical term be true in turple
+		return [(term.originalWord,term in queryStr) for term in self.index.getDocumentOfId(id)[snippetsStart:snippetsEnd]] #excellent implemention:return list of truple make critical term be true in turple
 
-	def getDocumentText(self,id):
-		return self.forwardIndex[id]
-
-	def getUrl(self,id):  # here we load all data from files thus the type is string !
-		return self.urlToId[id]	
 
 def createIndexDir(storedDocumentDir,indexDir):
 	indexer = Indexer()
