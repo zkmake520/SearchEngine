550682bfb608964b12fee278eec7499e6e607478
kanzhou <564254501@qq.com>
2015-03-25 15:40:20 -0700

    finish the CrawlerAPI part

--- CrawlerAPI2.py
+++ CrawlerAPI2.py
@@ -6,19 +6,19 @@ import argparse
 import pickle
 import os.path 
 import time
-from CrawlerAPI import storeSub
+from util import storeSub
 from distutils.dir_util import mkpath
-def crawlWithTimeStamp(storeDir,timeInterval,subReddit):
+def crawlWithTimeStamp(storeDir,timeInterval,subReddit,startTimeStamp):
 	mkpath(storeDir,mode=0755)
 	r = praw.Reddit(user_agent='ZKBot v0.2 by/u/kanzhou')
 	idToUrl,subIdDict,authorSubCount = dict(),dict(),dict()
-	timeStampEnd = int(time.time())+7*3600  #different timestamp of UTC and losangeles
+	timeStampEnd = int(time.time())+7*3600 if startTimeStamp is None else int(startTimeStamp)  #different timestamp of UTC and losangeles
 	timeStampStart = timeStampEnd - timeInterval
 	while True:
 		submissions = r.search("timestamp:{}..{}".format(timeStampStart,timeStampEnd),subreddit=subReddit,syntax="cloudsearch")
-		storeSub(submissions,subIdDict,authorSubCount,idToUrl,storeDir)
-		logging.debug("Get {} submissions in timeInterval {}..{}".format(len(list(submissions)),timeStampStart,timeStampEnd))
-
+		subs = list(submissions)
+		logging.debug("Get {} submissions in timeInterval {}..{}".format(len(subs),timeStampStart,timeStampEnd))
+		storeSub(subs,subIdDict,authorSubCount,idToUrl,storeDir)
 		timeStampEnd = timeStampStart
 		timeStampStart = timeStampEnd - timeInterval
 
@@ -29,7 +29,8 @@ def main():
 	argparser.add_argument("--storeDir", dest = "storeDir",required=True)
 	argparser.add_argument("--timeInterval", dest = "timeInterval",type = int,required=True)
 	argparser.add_argument("--subReddit", dest = "subReddit",required=True)
+	argparser.add_argument("--startTimeStamp", dest = "startTimeStamp",required=False,default=None)
 	args = argparser.parse_args()
-	crawlWithTimeStamp(args.storeDir,args.timeInterval,args.subReddit)
+	crawlWithTimeStamp(args.storeDir,args.timeInterval,args.subReddit,args.startTimeStamp)
 if __name__ == "__main__":   # if invoke from command line
 	main()
